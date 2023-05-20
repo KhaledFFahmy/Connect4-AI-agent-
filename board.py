0a9a4c5876cd5025b52fdb4dea0057501df110bb
Hassan Tamer <125097639+hassantamer20@users.noreply.github.com>
2023-05-20 18:30:58 +0300

    Update board.py
    Added Comment to Explain The Function

--- board.py
+++ board.py
@@ -16,7 +16,9 @@ BLUE = 2
 class Board:
     def __init__(self):
         self.board = [[EMPTY for i in range(7)] for j in range(6)]
-
+    
+    # The function print_grid takes in one parameter:
+    # grid, which is a two-dimensional list representing the color values of the cells in the game board.
     def print_grid(self, grid):
         for i in range(0, len(grid)):
             for j in range(0, len(grid[i])):
@@ -27,7 +29,9 @@ class Board:
                 elif grid[i][j] == BLUE:
                     print("B", end=" \t")
             print("\n")
-
+    
+    # The function _convert_grid_to_color takes in one parameter:
+    # grid, which is a two-dimensional list representing the color values of the cells in the game board in the form of RGB tuples.
     def _convert_grid_to_color(self, grid):
         for i in range(0, len(grid)):
             for j in range(0, len(grid[i])):
@@ -38,7 +42,9 @@ class Board:
                 elif grid[i][j][0] > 50:
                     grid[i][j] = BLUE
         return grid
-
+    
+    # The function _get_grid_cordinates has no parameters and returns a list of tuples representing 
+    # the screen coordinates of the top-left corner of each cell in the game board.
     def _get_grid_cordinates(self):
         startCord = (55, 55)
         cordArr = []
@@ -49,14 +55,20 @@ class Board:
                 cordArr.append((x, y))
         return cordArr
 
+    # The function _transpose_grid takes in one parameter: grid, 
+    # which is a two-dimensional list of integers representing the color values of the cells in the game board.
     def _transpose_grid(self, grid):
         return [[grid[j][i] for j in range(len(grid))] for i in range(len(grid[0]))]
 
+    # The function _capture_image has no parameters and returns a cropped image of the game board, 
+    # which is obtained by taking a screenshot of the entire screen and cropping it to include only the region where the game board is located.
     def _capture_image(self):
         image = ImageGrab.grab()
         cropedImage = image.crop((LEFT, TOP, RIGHT, BOTTOM))
         return cropedImage
 
+    # The function _convert_image_to_grid takes one parameter:
+    # image, which is a PIL Image object representing a screenshot of the game board.
     def _convert_image_to_grid(self, image):
         pixels = [[] for i in range(7)]
         i = 0
@@ -67,6 +79,10 @@ class Board:
             pixels[i].append(pixel)
         return pixels
 
+    # The function _get_grid has no parameters and returns a two-dimensional list representing the color values of the cells in the game board,
+    # which is obtained by capturing a screenshot of the game board, 
+    # converting it to a grid of color values, 
+    # transposing the grid, and returning the resulting grid.
     def _get_grid(self):
         cropedImage = self._capture_image()
         pixels = self._convert_image_to_grid(cropedImage)
@@ -74,6 +90,8 @@ class Board:
         grid = self._transpose_grid(pixels)
         return grid
 
+    # The function _check_if_game_end takes in one parameter: grid, 
+    # which is a two-dimensional list representing the current state of the game board.
     def _check_if_game_end(self, grid):
         for i in range(0, len(grid)):
             for j in range(0, len(grid[i])):
@@ -81,6 +99,9 @@ class Board:
                     return True
         return False
 
+    # The function get_game_grid has no parameters and returns a tuple containing two elements:
+    # The first element is a two-dimensional list representing the color values of the cells in the game board.
+    # The second element is a boolean value indicating whether the game has ended.
     def get_game_grid(self):
         game_grid = self._get_grid()
         new_grid = self._convert_grid_to_color(game_grid)
@@ -88,6 +109,8 @@ class Board:
         self.board = new_grid
         return (self.board, is_game_end)
 
+    # The function available_moves takes in one parameter: grid, 
+    # which is a two-dimensional list representing the current state of the game board.
     def available_moves(self, grid):
         moves = []
         for i in range(7):
@@ -95,18 +118,28 @@ class Board:
                 moves.append(i)
         return moves
 
+    # The function make_move takes in three parameters: grid, move, and player. 
+    # grid is a two-dimensional list representing the current state of the game board, 
+    # move is an integer representing the column index where the player wants to drop their token, 
+    # and player is an integer or a character representing the player's token color.
     def make_move(self, grid, move, player):
         for i in range(5, -1, -1):
             if grid[i][move] == EMPTY:
                 grid[i][move] = player
                 break
 
+    # The function undo_move takes in two parameters: grid and move. 
+    # grid is a two-dimensional list representing the current state of the game board, 
+    # and move is an integer representing the column index where the last token was dropped.
     def undo_move(self, grid, move):
         for i in range(6):
             if grid[i][move] != EMPTY:
                 grid[i][move] = EMPTY
                 break
 
+    # The function evaluate_window takes two parameters:
+    # window, which is a list of four tokens representing a window of cells in the game board.
+    # player, which is a string representing the color of the token of the current player.
     def evaluate_window(self, window, player):
         score = 0
         opponent = RED if player == BLUE else BLUE
@@ -154,6 +187,7 @@ class Board:
 
         return score
 
+    # MinMax Algorithm 
         def minimax(self, grid, depth, alpha, beta, maximizing_player):
             valid_moves = self.available_moves(grid)
             game_end = self._check_if_game_end(grid)
@@ -198,7 +232,8 @@ class Board:
                         break
                 return best_move
 			
-			
+	# The function select_column takes in one parameter:
+    # column, which is an integer representing the index of the column where the player wants to drop their token.		
     def select_column(self, column):
         pyautogui.click(
             self._get_grid_cordinates()[column][0] + LEFT,
